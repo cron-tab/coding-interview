5a858015c725cd916def27e338d8d45875fb4080
sd.baek <sd.baek@yanolja.com>
2023-06-01 09:36:24 +0900

    init

--- packages/test/230601_이지윤님/refactoring/refactoring.js
+++ packages/test/230601_이지윤님/refactoring/refactoring.js
@@ -1,4 +1,4 @@
-function printOwning(invoice) {
+function printOwing(invoice) {
     printBanner()
     let outstanding = calculateOutstanding()
 
@@ -7,37 +7,22 @@ function printOwning(invoice) {
     console.log(`amount: ${outstanding}`)
 }
 
-function printNameAndAmount({ customer }) {
-    printBanner(); //
-    let outstanding = calculateOutstanding();
+function printOwing({customer}) {
 
-    // print details
-    console.log(`name: ${invoice.customer}`)
-    console.log(`amount: ${outstanding}`)
 }
-
-print고객_이름_양({  })
 
 
 //------------------------------------------------------------
 
 
-function getPrice(order) {
+function getPrice() {
     return order.quantity * order.itemPrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
         Math.min(order.quantity * order.itemPrice * 0.1, 100)
 }
 
-function getPrice(order){
-    const { quantity, itemPrice } = order;
+function getPrice(){
 
-
-    return quantity * itemPrice
-}
-
-function getDiscount(order) {
-    return Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(order.quantity * order.itemPrice * 0.1, 100)
 }
 
 
@@ -63,30 +48,21 @@ function getPayAmount() {
 }
 
 function getPayAmount(){
-    if (isDead) {
-        return deadAmount();
-    }
 
-    if (isSeparated) {
-        return se();
-    }
-
-    if ()
 }
-
-function isDeadAmout()
 
 //------------------------------------------------------------
 
 
 function setDimension(name, value) {
-    Object(obj).
-    this[name] =
+    if (name === 'height') {
+        this._height = value
+        return
+    }
+    if (name === 'width') {
+        this._width = value
+        return
+    }
 }
-
-
-
-
-
 
 
