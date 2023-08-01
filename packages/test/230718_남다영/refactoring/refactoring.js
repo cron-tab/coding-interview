fd2c9643724ac2901eb42eadfd62f7a00b1f315f
sd.baek <sd.baek@yanolja.com>
2023-08-01 09:59:50 +0900

    init

---
+++ packages/test/230718_남다영/refactoring/refactoring.js
@@ -0,0 +1,59 @@
+function getPrice() {
+    return order.quantity * order.itemPrice -
+        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        Math.min(order.quantity * order.itemPrice * 0.1, 100)
+}
+
+function getPrice(){
+const { quantity, itemPrice, } = order
+    const
+        retun ()
+
+}
+
+
+//------------------------------------------------------------
+
+
+function getPayAmount() {
+    let result
+    if (isDead) {
+       return deadAmount()
+    }
+
+    if (isSeparated) {
+            return separatedAmount()
+        }
+    if(is)
+
+            if (isRetired) {
+                result = retiredAmount()
+            } else {
+                result = normalPayAmount()
+            }
+
+    return result
+}
+
+function getPayAmount(){
+
+}
+
+//------------------------------------------------------------
+
+
+function setDimension(name, value) {
+    if (name === 'height') {
+        this._height = value
+        return
+    }
+    if (name === 'width') {
+        this._width = value
+        return
+    }
+}
+
+function setDimension(name, value) {
+
+}
+
