9b89b1e24ac7e7dfe9f4608d1ae3f943c3db118c
sd.baek <sd.baek@yanolja.com>
2023-08-31 09:39:17 +0900

    init

---
+++ packages/test/230831_김성진/refactoring/refactoring.js
@@ -0,0 +1,82 @@
+function getPrice() {
+    return order.quantity * order.itemPrice -
+        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        Math.min(order.quantity * order.itemPrice * 0.1, 100)
+}
+
+
+const getPrice =()=> {
+
+    const a = order.qu
+    const b= order. p
+
+    const max =
+        const min =
+            const result = a+b max min
+            return resutl
+}
+    order.quantity * order.itemPrice -
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    Math.min(order.quantity * order.itemPrice * 0.1, 100)
+
+
+function getPrice(){
+
+}
+
+
+//------------------------------------------------------------
+
+
+function getPayAmount() {
+    let result
+    if (isDead) {
+        result = deadAmount()
+    } else {
+        if (isSeparated) {
+            result = separatedAmount()
+        } else {
+            if (isRetired) {
+                result = retiredAmount()
+            } else {
+                result = normalPayAmount()
+            }
+        }
+    }
+    return result
+
+}
+
+
+
+function getPayAmount(){
+
+}
+
+const getpayAmout = ()=>{
+
+    case
+        switch ()
+
+        raes
+
+}
+
+//------------------------------------------------------------
+
+
+function setDimension(name, value) {
+    if (name === 'height') {
+        this._height = value
+        return
+    }
+    if (name === 'width') {
+        this._width = value
+        return
+    }
+}
+
+function setDimension(name, value) {
+
+}
+
